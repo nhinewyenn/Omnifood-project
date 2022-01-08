b92407d7d616c74acf6dae49722e80ac1c6ce22b
Nhi Nguyen <85110195+nhinewyenn@users.noreply.github.com>
2022-01-09 00:29:44 +1000

    Add files via upload

---
+++ 08-Omnifood-project/Javascript/script.js
@@ -0,0 +1,99 @@
+console.log("hello world")
+const myName = ("Nhi Nguyen")
+const h1 = document.querySelector(".heading-primary")
+console.log(myName)
+console.log(h1)
+
+///////////////////////////////////////////////////////////
+// SET CURRENT YEAR
+const yearEL = document.querySelector(".year")
+const currentYear = new Date().getFullYear();
+console.log("currentYear")
+yearEL.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+// MAKE MOBILE NAVIGATION WORK
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header")
+btnNavEl.addEventListener('click', function() {
+headerEl.classList.toggle("nav-open")
+});
+
+///////////////////////////////////////////////////////////
+// SMOOTH SCROLLING ANIMATION
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky Navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const observer = new IntersectionObserver(function(entries){
+  const ent = entries[0];
+  console.log(ent);
+
+  if(ent.isIntersecting === false) {
+  document.body.classList.add("sticky");
+  }
+
+  if(ent.isIntersecting === true) {
+  document.body.classList.remove("sticky");
+  }
+}, 
+{
+  // in the viewport
+  root: null,
+  threshold: 0,
+  rootMargin: "-80px",
+
+});
+observer.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
+
+/*
+
+*/
---
+++ 08-Omnifood-project/readme.md
@@ -0,0 +1,3 @@
+# Omnifood website project (responsive)
+## HTML, CSS, JS
+This is an inspired website based on hellofresh to do as a mock website. This website is built using html, css and javascript
